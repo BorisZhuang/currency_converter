86341ab84cd5abf214cec6397e7666b7723f3f6b
Boris Zhuang <BZhuang@Boriss-MacBook-Air.local>
2018-07-06 22:18:10 -0700

    Home Screen: Style Text

--- currency-converter/app/screens/Home.js
+++ currency-converter/app/screens/Home.js
@@ -1,30 +1,33 @@
-import React, { Component } from 'react';
-import { StatusBar } from 'react-native';
-import { Container } from '../components/Container';
-import { Logo } from '../components/Logo';
-import { InputWithButton } from '../components/TextInput';
-import { ClearButton } from '../components/Buttons';
+import React, { Component } from "react";
+import { StatusBar } from "react-native";
+import { Container } from "../components/Container";
+import { Logo } from "../components/Logo";
+import { InputWithButton } from "../components/TextInput";
+import { ClearButton } from "../components/Buttons";
+import { LastConverted } from "../components/Text";
 
-const INIT_BASE_CURRENCY = 'USD';
-const INIT_QUOTE_CURRENCY = 'GBP';
-const INIT_BASE_PRICE = '100';
-const INIT_QUOTE_PRICE = '79.74';
+const INIT_BASE_CURRENCY = "USD";
+const INIT_QUOTE_CURRENCY = "GBP";
+const INIT_BASE_PRICE = "100";
+const INIT_QUOTE_PRICE = "79.74";
+const INIT_CONVERSION_RATE = 0.7974;
+const INIT_CONVERSION_DATE = new Date();
 
 class Home extends Component {
   handlePressBaseCurrency = () => {
-    console.log('base pressed.');
+    console.log("base pressed.");
   };
 
   handlePressQuoteCurrency = () => {
-    console.log('quote pressed.');
+    console.log("quote pressed.");
   };
 
-  handleTextChange = (text) => {
-    console.log('text changed', text);
+  handleTextChange = text => {
+    console.log("text changed", text);
   };
 
   handleSwapCurrency = () => {
-    console.log('swap currency pressed.');
+    console.log("swap currency pressed.");
   };
 
   render() {
@@ -45,7 +48,16 @@ class Home extends Component {
           value={INIT_QUOTE_PRICE}
           editable={false}
         />
-        <ClearButton text="Reverse Currencies" onPress={this.handleSwapCurrency} />
+        <LastConverted
+          base={INIT_BASE_CURRENCY}
+          quote={INIT_QUOTE_CURRENCY}
+          date={INIT_CONVERSION_DATE}
+          conversionRate={INIT_CONVERSION_RATE}
+        />
+        <ClearButton
+          text="Reverse Currencies"
+          onPress={this.handleSwapCurrency}
+        />
       </Container>
     );
   }
