ee8e61c54dfa3cdd7fceda34abff247a26bd8822
Boris Zhuang <BZhuang@Boriss-MacBook-Air.local>
2018-07-07 13:51:58 -0700

    Home Screen: Logo Animation.

--- currency-converter/app/components/Logo/styles.js
+++ currency-converter/app/components/Logo/styles.js
@@ -1,26 +1,30 @@
-import { Dimensions } from 'react-native';
-import EStyleSheet from 'react-native-extended-stylesheet';
+import { Dimensions } from "react-native";
+import EStyleSheet from "react-native-extended-stylesheet";
 
-const imageWidth = Dimensions.get('window').width / 2;
+const imageWidth = Dimensions.get("window").width / 2;
 
 export default EStyleSheet.create({
+  $largeContainerSize: imageWidth,
+  $largeImageSize: imageWidth / 2,
+  $smallContainerSize: imageWidth / 2,
+  $smallImageSize: imageWidth / 4,
   container: {
-    alignItems: 'center',
+    alignItems: "center"
   },
   containerImage: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: imageWidth,
-    height: imageWidth,
+    alignItems: "center",
+    justifyContent: "center",
+    width: "$largeContainerSize",
+    height: "$largeContainerSize"
   },
   image: {
-    width: imageWidth / 2,
+    width: "$largeImageSize"
   },
   text: {
-    fontWeight: '600',
+    fontWeight: "600",
     fontSize: 20,
     letterSpacing: -0.5,
     marginTop: 15,
-    color: '$white',
-  },
+    color: "$white"
+  }
 });
